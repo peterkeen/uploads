c6ef04777565747afe93ad19e40cbb03d1a81904
Pete Keen <peter.keen@bugsplat.info>
2013-01-11 08:44:03 -0800

    Generate http urls

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class Uploads < Sinatra::Base
         "name" => File.basename(filename),
         "mtime" => File.mtime(filename),
         "size" => File.size?(filename),
-        "url" => request.base_url + "/files/#{dirname}/#{File.basename(filename)}"
+        "url" => "http://#{request.host}/files/#{dirname}/#{File.basename(filename)}"
       }
     end
 
